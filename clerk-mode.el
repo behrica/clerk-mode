e2fc4fdcbad4820cef1505f0d003f5a84fc2643b
Carsten Behring <carsten.behring@gmail.com>
2023-02-06 21:32:35 +0100

    added tap inspector related functions

--- clerk-mode.el
+++ clerk-mode.el
@@ -39,6 +39,67 @@
     (cider-interactive-eval
      (concat "(nextjournal.clerk/show! \"" filename "\")"))))
 
+
+(defun clerk-get-current-viewers ()
+
+  (read
+   (nrepl-dict-get
+    (nrepl-sync-request:eval
+        "
+ (do
+ (require 'nextjournal.clerk)
+
+ (conj
+  (->>
+   (nextjournal.clerk/get-default-viewers)
+   (map :name)
+   (remove nil?))
+  :default))
+"
+     (cider-current-connection))
+    "value")))
+
+;;  Call while in `cider-inspector` to show current value in Cler after a viewer is selected
+(defun cider-inspector-tap-current-val-with-clerk-viewer (viewer)
+  (interactive
+   (list (completing-read "Choose viewer: " (clerk-get-current-viewers)
+                          nil t)))
+
+  (setq cider-inspector--current-repl (cider-current-repl))
+  ;; pick some random name, which hopeuflly is never used...
+  (when-let* ((ns "user")
+              (var-name "cider-inspector-temp-hdhsad-hbjdbasjd842342")
+              (value (cider-sync-request:inspect-def-current-val ns var-name)))
+
+    (let ((tapped-form (concat "(clojure.core/->> "
+                               (concat ns "/" var-name)
+                               (if (equal ":default" viewer)
+                                   (concat " (nextjournal.clerk/with-viewer {:transform-fn identity})")
+                                 (if (string-prefix-p ":" viewer)
+                                     (concat " (nextjournal.clerk/with-viewer " "(keyword \"" (substring viewer 1) "\")" ")")
+                                   (concat " (nextjournal.clerk/with-viewer " "(symbol \"" viewer "\")" ")"))
+                                 )
+
+                               " (clojure.core/tap>))")))
+      (cider-interactive-eval tapped-form
+                              nil
+                              nil
+                              (cider--nrepl-pr-request-map)))
+
+
+    (message "%s#'%s/%s = %s" cider-eval-result-prefix ns var-name value)))
+
+
+(defun clerk-open-tap-inspector ()
+  (interactive)
+  (cider-nrepl-sync-request:eval
+   (concat "(require '[nextjournal.clerk :as clerk])"
+           "(nextjournal.clerk/show! 'nextjournal.clerk.tap)"
+           "(clerk/serve! {:browse true})"
+
+           )))
+
+
 (define-minor-mode clerk-mode
   "Minor mode for presenting files in Clerk."
   :lighter " clerk"
